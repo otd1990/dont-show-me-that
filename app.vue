e984ec20e1bd7824409b02e08707d2143ffcfab1
Oliver Dyer <Oliver.Dyer@dominos.co.uk>
2024-04-05 08:55:06 +0100

    Hooking up API

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Don't show me that!</h1>
+    <div v-for="post in posts" :key="post.id">
+      {{ post.text }}
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+export type Post = {
+  id: String;
+  text: string;
+};
+
+const config = useRuntimeConfig();
+const posts = ref<Post[]>([]);
+
+onMounted(() => {
+  console.log(config);
+  fetch("https://dummyapi.io/data/v1/post", {
+    headers: {
+      "app-id": config.public.dummyApiId,
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => (posts.value = json.data));
+});
+</script>
